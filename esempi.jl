c57944d8495143f92c6b5123f820311eb4474321
marteresagh <marteresa28@gmail.com>
2018-03-13 09:12:21 +0100

    aggiornato con commenti

--- esempi.jl
+++ esempi.jl
@@ -13,40 +13,70 @@ mod_2 = larSplit(3)(3), Cell(3)(1)
 squares = larModelProduct(mod_2,mod_2)
 VIEW_EXPLODE(squares)
 cubes = larModelProduct(squares,mod_2)
+#numero dei vertici
+vertici=size(cubes[1],2)
+#numero dei cubetti
+cubi=length(cubes[2])
 VIEW_EXPLODE(cubes)
 
-#estrazione delle facce
+#estrazione delle facce con larCuboidsFacets
 CUBE=larCuboids([1,1,1])
-FACES=larCuboidsFacets(CUBE[1],CUBE[2])
-mod_3=CUBE[1],FACES[2]
+V,FACES=larCuboidsFacets(CUBE[1],CUBE[2])
+length(FACES)
+mod_3=V,FACES
 VIEW_EXPLODE(mod_3)
-simpli=larSimplicialStack(FACES[2])[2]
-mod_4=CUBE[1],simpli
-VIEW_EXPLODE(mod_4)
 
-#estrazione degli spigoli	
-EDGE=larGridSkeleton([1,1,1])(1)
-mod_5=CUBE[1],EDGE
-VIEW_EXPLODE(mod_5)
+#visualizzazione dei 0-,1-,2-,3-scheletro con larGridSkeleton
 
-#visualizzazione dei 0-,1-,2-,3-scheletro
-
-#0-scheletro
-point=CUBE[1],gridSkeletons([1,1,1])[1]
-VIEW_EXPLODE(point)
+#0-scheletro 
+point=CUBE[1],larGridSkeleton([1,1,1])(0)
+length(point[2])
+VIEW(point)
 
 #1-scheletro
-edge=CUBE[1],gridSkeletons([1,1,1])[2]
+edge=CUBE[1],larGridSkeleton([1,1,1])(1)
+length(edge[2])
 VIEW_EXPLODE(edge)
 
 #2-scheletro
-face=CUBE[1],gridSkeletons([1,1,1])[3]
+face=CUBE[1],larGridSkeleton([1,1,1])(2)
+length(face[2])
 VIEW_EXPLODE(face)
 
 #3-scheletro
-cell=CUBE[1],gridSkeletons([1,1,1])[4]
+cell=CUBE[1],larGridSkeleton([1,1,1])(3)
+length(cell[2])
 VIEW_EXPLODE(cell)
 
-#numerazione elementi
-edge1=CUBE[1],gridSkeletons([1,1,1])
-VIEW_NUMBERED(edge1)
+#numerazione elementi di tutti gli scheletri
+schel1=CUBE[1],gridSkeletons([1,1,1])
+VIEW_NUMBERED(schel1)
+
+#esempio di visualizzazione triangoli e tetraedri
+v,ev=[0 1 0; 0 0 1],[[1,2],[2,3],[1,3]]
+tri_edge=v,ev
+VIEW(tri_edge)
+
+v,fv=[0 1 0; 0 0 1],[[1,2,3]]
+triangle=v,fv
+VIEW(triangle)
+
+#1-scheletro
+vv,evv=[0 1 0 0 ; 0 0 1 0;0 0 0 1],larSimplicialStack([[1,2,3,4]])[2]
+tetra_edge=vv,evv
+VIEW(tetra_edge)
+
+#2-scheletro
+vv,fvv=[0 1 0 0 ; 0 0 1 0;0 0 0 1],larSimplicialStack([[1,2,3,4]])[3]
+tetra_face=vv,fvv
+VIEW_EXPLODE(tetra_face)
+
+#3-scheletro
+vv,cvv=[0 1 0 0 ; 0 0 1 0;0 0 0 1],[[1,2,3,4]]
+tetra=vv,cvv
+VIEW(tetra)
+
+#numerazione usando larSimplicialStack
+simpli=larSimplicialStack(cvv)
+schel2=vv,simpli
+VIEW_NUMBERED(schel2)
